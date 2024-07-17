eb42e1b0d17cf31a7ab473d0dcc9159f21b38f11
chinhbean09 <chinh0726@gmail.com>
2024-07-18 00:03:40 +0700

    add exception

--- src/main/java/com/chinhbean/bookinghotel/enums/BookingStatus.java
+++ src/main/java/com/chinhbean/bookinghotel/enums/BookingStatus.java
@@ -1,11 +1,21 @@
     package com.chinhbean.bookinghotel.enums;
 
     public enum BookingStatus {
-        PENDING, CONFIRMED, PAID, CHECKED_IN, CHECKED_OUT, CANCELLED
         //PENDING: Đặt phòng đã được tạo nhưng chưa được xác nhận hoặc thanh toán.
-        //CONFIRMED: Đặt phòng đã được xác nhận bởi nhà cung cấp dịch vụ.
+
+        PENDING
         //PAID: Đặt phòng đã được thanh toán.
+
+        ,PAID,
         //CHECKED_IN: Khách hàng đã nhận phòng.
+
+        CHECKED_IN,
         //CHECKED_OUT: Khách hàng đã trả phòng.
+
+        CHECKED_OUT,
         //CANCELLED: Đặt phòng đã bị hủy bởi khách hàng hoặc nhà cung cấp dịch vụ.
+
+        CANCELLED
+
+
     }
